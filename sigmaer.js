24b05d605f7179c8f2bc8b7b360379b89a7c6c14
Fletcher <105764657@student.swin.edu.au>
2025-03-13 22:59:52 +1100

    God fund me

--- sigmaer.js
+++
@@ -1,8 +0,0 @@
-function changeImage(img) {
-    img.src = 'until-then-sigma.gif';  // Image to show when hovered
-  }
-  
-  function restoreImage(img) {
-    img.src = 'until-then-until-then-computer.gif';  // Original image
-  }
-  
